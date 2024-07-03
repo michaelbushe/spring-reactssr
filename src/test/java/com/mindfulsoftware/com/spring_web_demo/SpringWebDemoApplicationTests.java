6842c85806ed49b244bf30bc890326fd0f9d6b60
Michael Bushe <michael@bushe.com>
2024-07-03 19:35:16 -0400

    Inital app with index and error

---
+++ src/test/java/com/mindfulsoftware/com/spring_web_demo/SpringWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindfulsoftware.com.spring_web_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
